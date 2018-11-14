a0fe144bdc666defafa11a593b2a4543c9b6ee56
Nathaniel Brough <nathaniel.brough@gmail.com>
2018-11-14 19:57:10 +0800

    Binary and library target rules ready

--- internal/templates.go
+++ internal/templates.go
@@ -9,14 +9,14 @@ import (
 
 // CC rule string templates
 const (
-	ccBinaryTemplate  = "{{.Comment}}\ncc_binary(\n{{.Operands}}\n)"
-	ccLibraryTemplate = "{{.Comment}}\ncc_binary(\n{{.Operands}}\n)"
-	ccImportTemplate  = "{{.Comment}}\ncc_binary(\n{{.Operands}}\n)"
+	ccBinaryTemplate  = "{{.Comment}}\ncc_binary({{.Operands}})\n"
+	ccLibraryTemplate = "{{.Comment}}\ncc_library({{.Operands}})\n"
+	ccImportTemplate  = "{{.Comment}}\ncc_import({{.Operands}})\n"
 )
 
 // Operand templates
 const (
-	operandTemplate = "  {{.Operand}}={{.Value}},{{.Comment}}\n"
+	operandTemplate = "{{.Operand}}={{.Value}},{{.Comment}}"
 )
 
 // Common Attribute List
@@ -53,14 +53,19 @@ const (
 	attStripIncludePrefix = "strip_include_prefix"
 )
 
+// Comments
+type comment struct {
+	Comment string
+}
+
 // Bazel Types
 type bBool bool
 
 func (b bBool) String() string {
 	if b {
-		return "True"
+		return `"True"`
 	}
-	return "False"
+	return `"False"`
 }
 
 type bString string
@@ -72,47 +77,106 @@ func (s bString) String() string {
 type bStringList []string
 
 func (sList bStringList) String() string {
-	var output string
+	output := "["
 	for _, s := range sList {
-		output = "\"" + output + s
+		output = output + strconv.Quote(string(s)) + ","
 	}
+	output = output + "]"
 	return output
 }
 
-type parameter interface {
-	Parameter() operandBase
+type attribute interface {
+	Attribute() attributeBase
 }
 
-type operandBase struct {
+type attributeBase struct {
+	comment
 	Operand string
 	Value   string
 }
 
-func (op operandBase) String() string {
+func (at attributeBase) String() string {
 	templateName := "operand"
 	t := template.Must(template.New(templateName).Parse(operandTemplate))
 	var output bytes.Buffer
-	err := t.Execute(&output, op)
+	err := t.Execute(&output, at)
 	if err != nil {
 		log.Println("Executing template:", err)
 	}
 	return output.String()
 }
 
-type operandBString struct {
+type attributeBString struct {
+	comment
 	Operand string
 	Value   bString
 }
 
-func (op operandBString) Parameter() operandBase {
-	return operandBase{Operand: op.Operand, Value: op.Value.String()}
+func (at attributeBString) Attribute() attributeBase {
+	return attributeBase{Operand: at.Operand, Value: at.Value.String()}
 }
 
-type operandBBool struct {
+type attributeBStringList struct {
+	comment
+	Operand string
+	Value   bStringList
+}
+
+func (at attributeBStringList) Attribute() attributeBase {
+	return attributeBase{Operand: at.Operand, Value: at.Value.String(), comment: comment{at.Comment}}
+}
+
+type attributeBBool struct {
+	comment
 	Operand string
 	Value   bBool
 }
 
-func (op operandBBool) Parameter() operandBase {
-	return operandBase{Operand: op.Operand, Value: op.Value.String()}
+func (at attributeBBool) Attribute() attributeBase {
+	return attributeBase{Operand: at.Operand, Value: at.Value.String(), comment: comment{at.Comment}}
+}
+
+type attributeList []attribute
+
+func (list attributeList) String() string {
+	var output string
+	for _, s := range list {
+		output += s.Attribute().String()
+	}
+	return output
+}
+
+type rule struct {
+	comment
+	Operands attributeList
+}
+
+type ccLibraryRule struct {
+	rule
+}
+
+func (r ccLibraryRule) String() string {
+	templateName := "cc_library"
+	t := template.Must(template.New(templateName).Parse(ccLibraryTemplate))
+	var output bytes.Buffer
+	err := t.Execute(&output, r)
+	if err != nil {
+		log.Println("Executing template:", err)
+	}
+	return output.String()
+}
+
+type ccBinaryRule struct {
+	rule
+}
+
+func (r ccBinaryRule) String() string {
+	templateName := "cc_binary"
+	t := template.Must(template.New(templateName).Parse(ccBinaryTemplate))
+	var output bytes.Buffer
+	err := t.Execute(&output, r)
+	if err != nil {
+		log.Println("Executing template:", err)
+	}
+	return output.String()
 }
--- internal/templates_test.go
+++ internal/templates_test.go
@@ -4,34 +4,95 @@ import (
 	"testing"
 )
 
-func TestOperandBStringParameter(t *testing.T) {
-	op := operandBString{Operand: "name", Value: "hello_world"}
-	expected := "  name=\"hello_world\",\n"
-	got := op.Parameter().String()
+func TestAttributeBString(t *testing.T) {
+	op := attributeBString{Operand: "name", Value: "hello_world"}
+	expected := `name="hello_world",`
+	got := op.Attribute().String()
 	if expected != got {
 		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
 	}
 }
-func TestOperandBBoolParameter(t *testing.T) {
-	op := operandBBool{Operand: "linkstatic", Value: true}
-	expected := "  linkstatic=True,\n"
-	got := op.Parameter().String()
+
+func TestBStringList(t *testing.T) {
+	var list bStringList = []string{"a", "b", "c"}
+	expected := `["a","b","c",]`
+	got := list.String()
+	if expected != got {
+		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
+	}
+}
+
+func TestAttributeBBool(t *testing.T) {
+	op := attributeBBool{Operand: "linkstatic", Value: true}
+	expected := `linkstatic="True",`
+	got := op.Attribute().String()
 	if expected != got {
 		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
 	}
 }
 func TestBBoolString(t *testing.T) {
 	var toggle bBool = true
-	expected := "True"
+	expected := `"True"`
 	got := toggle.String()
 	if expected != got {
 		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
 	}
 	toggle = false
-	expected = "False"
+	expected = `"False"`
 	got = toggle.String()
 	if expected != got {
 		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
 	}
+}
 
+func TestAttributeStringList(t *testing.T) {
+	var list bStringList = []string{"a", "b", "c"}
+	srcs := attSrcs
+	expected := `srcs=["a","b","c",],`
+	strList := attributeBStringList{Operand: srcs, Value: list}
+	got := strList.Attribute().String()
+	if expected != got {
+		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
+	}
+}
+
+func TestAttributeListString(t *testing.T) {
+	name := attributeBString{Operand: "name", Value: "hello_world"}
+	linkOpt := attributeBBool{Operand: "linkstatic", Value: true}
+	expected := name.Attribute().String() + linkOpt.Attribute().String()
+	var list attributeList = []attribute{name, linkOpt}
+	got := list.String()
+	if expected != got {
+		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
+	}
+}
+
+func TestCCLibraryString(t *testing.T) {
+	name := attributeBString{Operand: "name", Value: "hello_world"}
+	linkOpt := attributeBBool{Operand: "linkstatic", Value: true}
+	var srclist bStringList = []string{"a", "b", "c"}
+	srcs := attributeBStringList{Operand: attSrcs, Value: srclist}
+	var list attributeList = []attribute{name, srcs, linkOpt}
+	ccLibrary := ccLibraryRule{rule{Operands: list, comment: comment{Comment: "#Hello test"}}}
+	expected := "#Hello test\ncc_library(name=\"hello_world\",srcs=[\"a\",\"b\",\"c\",],linkstatic=\"True\",)\n"
+	got := ccLibrary.String()
+
+	if expected != got {
+		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
+	}
+}
+
+func TestCCBinaryString(t *testing.T) {
+	name := attributeBString{Operand: "name", Value: "hello_world"}
+	linkOpt := attributeBBool{Operand: "linkstatic", Value: true}
+	var srclist bStringList = []string{"a", "b", "c"}
+	srcs := attributeBStringList{Operand: attSrcs, Value: srclist}
+	var list attributeList = []attribute{name, srcs, linkOpt}
+	ccLibrary := ccBinaryRule{rule{Operands: list, comment: comment{Comment: "#Hello test"}}}
+	expected := "#Hello test\ncc_binary(name=\"hello_world\",srcs=[\"a\",\"b\",\"c\",],linkstatic=\"True\",)\n"
+	got := ccLibrary.String()
+
+	if expected != got {
+		t.Errorf("Expected:\n%#v \nGot:\n%#v \n", expected, got)
+	}
 }
